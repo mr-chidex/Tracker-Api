674f3db3af81c8f7e0ea293f980e3c28dad6cc69
mr-chidex <mbahchiboy2@gmail.com>
2022-04-06 10:24:18 +0100

    track endpoint concluded

--- src/controllers/tracks.ts
+++ src/controllers/tracks.ts
@@ -1,6 +1,8 @@
-import { Request, Response, RequestHandler } from "express";
+import { Request, Response } from "express";
+import { TrackDoc } from "../libs/types";
 
 import { Track } from "../models/tracks";
+import { ValidateTrack } from "../validators/tracks";
 
 /**
  * @route - GET api/v1/tracks
@@ -14,4 +16,32 @@ export const getTracks = async (req: Request | any, res: Response) => {
   res.json({ status: "sucess", tracks });
 };
 
-export const addTrack: RequestHandler = async (req, res) => {};
+/**
+ * @route - POST api/v1/tracks
+ * @access - Private
+ * @description - add track for a user
+ * Note: req: Request | any - was use because we need req.user
+ * @required - name, location
+ */
+export const addTrack = async (req: Request | any, res: Response) => {
+  const { error, value } = ValidateTrack(req.body);
+
+  //check for errors
+  if (error)
+    return res.status(422).json({
+      status: "error",
+      message: error.details[0].message,
+    });
+
+  const { name, locations }: TrackDoc = value;
+
+  const track = new Track({ name, locations, user: req.user?._id });
+
+  await track.save();
+
+  res.status(201).json({
+    status: "success",
+    message: "track added successfully",
+    track,
+  });
+};
--- src/routes/tracks.ts
+++ src/routes/tracks.ts
@@ -1,11 +1,12 @@
 import expressPromise from "express-promise-router"; //allows you omit try catch  on controllers
 
-import { getTracks } from "../controllers/tracks";
+import { addTrack, getTracks } from "../controllers/tracks";
 import auth from "../middlewares/auth";
 
 const router = expressPromise();
 
 router.use(auth);
 router.route("/").get(getTracks);
+router.route("/").post(addTrack);
 
 export default router;
